cc2f410cac89d4e720aca39403d7d56dfac58f2e
andersonkwasi <kwasianderson01@gmail.com>
2024-12-19 22:10:21 +0000

    Config vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true, // Permet d'accepter les connexions externes
-    port: 5173, // Assurez-vous que le port 5173 est configuré
+  build: {
+    outDir: 'dist', // Permet d'accepter les connexions externes
   },
+
+
+  //server: {
+    // host: true, // Permet d'accepter les connexions externes
+    // port: 5173, // Assurez-vous que le port 5173 est configuré
+  //},
 })
